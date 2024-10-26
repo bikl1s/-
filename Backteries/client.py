2af5040ea49cc66f569711fe12efbeb08e601a18
bikl1s <sergey.avalon01@gmail.com>
2024-10-26 10:49:33 +0300

    окно входа в игру, отрисовка других бактерий отправка данных о имени и цвете

--- Backteries/client.py
+++ Backteries/client.py
@@ -1,10 +1,79 @@
 import socket
 import pygame
 import math
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox
 
+
+def find(vector: str):
+    first = None
+    for num, sign in enumerate(vector):
+        if sign == "<":
+            first = num
+        if sign == ">" and first is not None:
+            second = num
+            result = vector[first + 1:second]  # Поменяли
+            return result
+    return ""
+
+
+def draw_bacteries(data: list[str]):
+    for num, bact in enumerate(data):
+        data = bact.split(" ")  # Разбиваем по пробелам подстроку одной бактерии
+        x = CC[0] + int(data[0])
+        y = CC[1] + int(data[1])
+        size = int(data[2])
+        color = data[3]
+        pygame.draw.circle(screen, color, (x, y), size)
+
+
+def login():
+    global name
+    name = row.get()
+    if name and color:
+        root.destroy()
+        root.quit()
+    else:
+        tk.messagebox.showerror("Ошибка", "Ты не выбрал цвет или не ввёл имя!")
+
+
+def scroll(event):
+    global color
+    color = combo.get()
+    style.configure("TCombobox", fieldbackground=color, background="white")
+
+
+name = ""
+color = ""
+root = tk.Tk()
+root.title("Логин")
+root.geometry("300x200")
+style = ttk.Style()
+style.theme_use('default')
+name_label = tk.Label(root, text="Введи свой никнейм:")
+name_label.pack()
+row = tk.Entry(root, width=30, justify="center")
+row.pack()
+color_label = tk.Label(root, text="Выбери цвет:")
+color_label.pack()
+colors = ['Maroon', 'DarkRed', 'FireBrick', 'Red', 'Salmon', 'Tomato', 'Coral', 'OrangeRed', 'Chocolate', 'SandyBrown',
+          'DarkOrange', 'Orange', 'DarkGoldenrod', 'Goldenrod', 'Gold', 'Olive', 'Yellow', 'YellowGreen', 'GreenYellow',
+          'Chartreuse', 'LawnGreen', 'Green', 'Lime', 'SpringGreen', 'MediumSpringGreen', 'Turquoise',
+          'LightSeaGreen', 'MediumTurquoise', 'Teal', 'DarkCyan', 'Aqua', 'Cyan', 'DeepSkyBlue',
+          'DodgerBlue', 'RoyalBlue', 'Navy', 'DarkBlue', 'MediumBlue']
+
+combo = ttk.Combobox(root, values=colors, textvariable=color)
+combo.bind("<<ComboboxSelected>>", scroll)
+combo.pack()
+name_btn = tk.Button(root, text="Зайти в игру", command=login)
+name_btn.pack()
+root.mainloop()
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Настраиваем сокет
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)  # Отключаем пакетирование
 sock.connect(("localhost", 10000))
+# Отправляем цвет и имя
+sock.send(("color:<" + name + "," + color + ">").encode())
 pygame.init()
 WIDTH = 800
 HEIGHT = 600
@@ -33,10 +102,11 @@ while run:
     # получаем
     data = sock.recv(1024).decode()
     print("Получил:", data)
+    data = find(data).split(",")  # Разбиваем на шары
     screen.fill('gray')
-    pygame.draw.circle(screen, (255, 0, 0), CC, radius)
+    pygame.draw.circle(screen, color, CC, radius)
+    if data != ['']:
+        draw_bacteries(data)
     pygame.display.update()
 
-
 pygame.quit()
-
